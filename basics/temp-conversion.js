6811ef121feaa3aefcdb525fb810e10f9787ce75
v-flores <victoralejandroflores88@gmail.com>
2019-07-06 21:29:03 -0500

    first edit test

--- basics/temp-conversion.js
+++ basics/temp-conversion.js
@@ -4,8 +4,6 @@ let fahrenheit = 32
 let celsius = (fahrenheit-32) * (5/9)
 console.log(celsius)
 
-
-
 //calculate kevlin value and store in variable
 // print the value
 let kelvin = celsius + 273.15
