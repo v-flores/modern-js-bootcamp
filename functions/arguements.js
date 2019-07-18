20cb9187e83e1567fbc27c121e6a60b79e25c3dc
v-flores <44541135+v-flores@users.noreply.github.com>
2019-07-17 19:04:28 -0500

    arguments

---
+++ functions/arguements.js
@@ -0,0 +1,6 @@
+let add = function (a,b,c) {
+    return a + b + c 
+}
+
+let result = add(10,1, 16)
+console.log(result)
---
+++ functions/undefined-null.js
@@ -0,0 +1,35 @@
+//Undefind for variable
+
+let name
+
+name = 'Jen'
+
+if (name === undefined ){
+    console.log('Please provide a name')
+} else {
+    console.log(name)
+}
+
+
+
+
+
+//undefined for function arguments
+
+// undefined as function return default value
+
+
+let square = function(num){
+    console.log(num)
+}
+
+let result = square()
+
+console.log(result)
+
+// 
+let age = 27
+
+age = null
+
+console.log(age)
